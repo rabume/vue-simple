da6166d9a5dce3e53ef8e1923ad40341899680eb
rabume <info@rabu.me>
2022-12-26 18:46:28 +0100

    first commit

---
+++ registration-form.js
@@ -0,0 +1,73 @@
+const RegistartionForm = {
+    data() {
+        return {
+            addressSameChecked: true
+        }
+    },
+    props: ["items"],
+    template: `
+        <h3>Registration</h3>
+        <hr>
+        <form autocomplete="off" class="needs-validation" novalidate>
+            <text-input label="First Name" name="first-name" required="true" type="text"></text-input>
+            <text-input label="Last Name" name="last-name" required="true" type="text"></text-input>
+            <text-input label="Email" name="email" required="true" type="email"></text-input>
+            <text-input label="Password" name="password" required="true" type="password"></text-input>
+            <select-input label="Favourite Color" name="color" :items="items"></select-input>
+
+            <text-input label="Address" name="adress" required="true" type="text"></text-input>
+            <text-input label="City/Town" name="city" required="true" type="text"></text-input>
+            <text-input label="State/Province" name="province" required="true" type="text"></text-input>
+            <text-input label="Zip/Postal" name="zip" required="true" type="text"></text-input>
+            
+            <check-input v-on:click="addressSame" label="Mailing Address Same" checked="true" v-model="addressSameChecked"></check-input>
+            
+            <div v-if="addressSameChecked === false">
+                <div class="mt-3">
+                    <text-input label="Mailing Address" name="adress2" type="text"></text-input>
+                    <text-input label="Mailing City/Town" name="city2" type="text"></text-input>
+                    <text-input label="Mailing State/Province" name="province2" type="text"></text-input>
+                    <text-input label="Mailing Zip/Postal" name="zip2" type="text"></text-input>
+                </div>
+            </div>
+            
+            <check-input label="I agree to terms and conditions" required="true"></check-input>
+            <hr>
+            <input class="btn btn-outline-primary" type="submit" value="Register">
+        </form>
+    `,
+    methods: {
+        addressSame() {
+            if (this.addressSameChecked === true) {
+                this.addressSameChecked = false
+            }else {
+                this.addressSameChecked = true
+            }
+        }
+    },
+    components: {
+        'text-input': TextInput,
+        'select-input': SelectInput,
+        'check-input': CheckInput,
+    },
+    mounted() {
+        (() => {
+            'use strict'
+
+            // Fetch all the forms we want to apply custom Bootstrap validation styles to
+            const forms = document.querySelectorAll('.needs-validation')
+
+            // Loop over them and prevent submission
+            Array.from(forms).forEach(form => {
+                form.addEventListener('submit', event => {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+
+                form.classList.add('was-validated')
+                }, false)
+            })
+        })()
+    }
+}
